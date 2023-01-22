b95d1f8e8fd37cd376d2e82b5bd514f16c1730b0
SHUBHAM KUMAR <72158457+shubham-01-star@users.noreply.github.com>
2023-01-22 21:55:50 +0530

    Add files via upload

---
+++ JavaBasics.java
@@ -0,0 +1,30 @@
+public class JavaBasics {
+
+    public static void main(String[] args) {
+        // System.out.print("Hello world\n");//output
+        // System.out.println("Hello world");//println line ka space
+        // System.out.print("Hello world");
+
+        /* pattern print */
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+       /*variable in java*/
+       int a= 10;
+       int b=20;
+       System.out.println(a);
+       System.out.println(b);
+       String name= "shubham";
+       a=50;
+       System.out.println(a);
+       System.out.println(name);
+       int sum= a+b;
+       System.out.println(sum);
+
+
+    }
+
+
+}// boilerplate code
+
